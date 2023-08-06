da373c494b3fb9725456f9fbbc23d235d50f6bcb
Phong Nguyen <nguyendoanphong.it@gmail.com>
2023-08-06 18:35:13 +0700

    duplicate files

---
+++ DuplicateFiles/Program.cs
@@ -0,0 +1,22 @@
+﻿
+var directory = args != null && args.Length > 0 ? args[0] : @"../test";
+var files = Directory.GetFiles(directory, "*", SearchOption.AllDirectories);
+
+foreach (var file in files)
+{
+    var fileInfor = new FileInfo(file);
+    var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(file);
+    if (fileNameWithoutExtension.EndsWith("_a"))
+    {
+        continue;
+    }
+
+    var copied = Path.Combine(fileInfor.DirectoryName!, Path.GetFileNameWithoutExtension(file) + "_a" + fileInfor.Extension);
+
+    if (File.Exists(copied))
+    {
+        continue;
+    }
+
+    File.Copy(file, copied);
+}
