278bfcdb83f13d4fe96e3164e5263d4679dfed41
Phong Nguyen <nguyendoanphong.it@gmail.com>
2026-04-25 13:37:27 +0700

    MarkItDown

---
+++ Toolbox.Plugins.MarkItDown.CliTool/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Net.Http.Headers;
+
+HttpClient client = new HttpClient();
+
+Console.WriteLine(args[0]);
+var filePath = args[0];
+
+if (Directory.Exists(filePath))
+{
+    foreach (var file in Directory.GetFiles(filePath))
+    {
+        await ConvertAsync(file);
+    }
+}
+else
+{
+    await ConvertAsync(filePath);
+}
+
+async Task ConvertAsync(string path)
+{
+    if (path.EndsWith(".md", StringComparison.OrdinalIgnoreCase))
+    {
+        Console.WriteLine($"File {path} is already a markdown file, skipping.");
+        return;
+    }
+
+    var resultFilePath = path + ".md";
+
+    using var form = new MultipartFormDataContent();
+    using var fileContent = new ByteArrayContent(File.ReadAllBytes(path));
+    fileContent.Headers.ContentType = MediaTypeHeaderValue.Parse("multipart/form-data");
+    form.Add(fileContent, "file", Path.GetFileName(path));
+    form.Add(new StringContent("Test Name"), "name");
+
+    var response = await client.PostAsync($"http://localhost:8000/convert", form);
+    response.EnsureSuccessStatusCode();
+
+    var markdown = await response.Content.ReadAsStringAsync();
+
+    File.WriteAllText(resultFilePath, markdown);
+
+    Console.WriteLine(markdown);
+}
